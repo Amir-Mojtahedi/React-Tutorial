42f1d83e8bdcce727fc0b8af54b40dd0da2733dd
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-08-20 11:37:41 -0400

    Added the css html and js files to begin the react project

---
+++ first_code/index.js
@@ -0,0 +1,20 @@
+const navbar = (
+    <nav>
+        <h1>Amir's Website</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+// function Navbar(){
+//     return(
+//         <nav>
+//             <h1 ClassName = 'navbar'>My navbar</h1>
+//         </nav>
+//     )
+// }
+
+ReactDOM.render(navbar,document.getElementById('root'))
